85dd356daf00d6e2a4381c206afe6ed0bb57937b
mahnaz-ahmadi <84898117+mahnaz-ahmadi@users.noreply.github.com>
2021-05-27 15:07:59 +0430

    Create Python-1400-03-06.py

---
+++ Python-1400-03-06.py
@@ -0,0 +1,8 @@
+print("lotfan adad aval vared konid")
+first_number = input()
+first_number_float = float(first_number)
+print("lotfan adad dovom vared konid")
+second_number = input()
+second_number_float = float(second_number)
+result = first_number_float + second_number_float
+print(result)
