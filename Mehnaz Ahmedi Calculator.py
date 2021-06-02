6726e79a79aaeaeb465e60d8387f63bdd97f5ace
mahnaz-ahmadi <84898117+mahnaz-ahmadi@users.noreply.github.com>
2021-06-02 16:06:06 +0430

    Update Mehnaz Ahmedi Calculator.py

--- Mehnaz Ahmedi Calculator.py
+++ Mehnaz Ahmedi Calculator.py
@@ -5,8 +5,6 @@ print("2=-")
 print("3=*")
 print("4=/")
 selection=input()
-# if selection > 4:
-#     print("fuck you")
 num1=float(input("Enter the First number"))
 num2=float(input("Enter the Second number"))
 if selection == '1':
@@ -17,5 +15,3 @@ elif selection == '3':
             print(num1, "*", num2, "=", num1*num2)
 elif selection == '4':
             print(num1, "/", num2, "=", num1/num2)
-#    else:
-#            print("Invalid Input")
