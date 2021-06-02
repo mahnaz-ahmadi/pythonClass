b709f2cc86d86e5def304b217e0d0363c41de991
mahnaz-ahmadi <84898117+mahnaz-ahmadi@users.noreply.github.com>
2021-06-02 18:57:28 +0430

    Update Mehnaz Ahmedi BMI.py

--- Mehnaz Ahmedi BMI.py
+++ Mehnaz Ahmedi BMI.py
@@ -1,6 +1,6 @@
 print("Let's Calculate Your BMI")
-W=float(input("Please Enter Your Weight (kg)"))
-H=float(input("Now Please Enter Your Height (m)"))
+W=float(input("Please Enter Your Weight (kg):"))
+H=float(input("Now Please Enter Your Height (m):"))
 BMI=int((W/H**2))
 if 15<BMI<25:
     print(f"Here is your BMI score: {BMI} \nYou're great")
