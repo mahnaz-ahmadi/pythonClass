fe20e5e5120a44b9999479be977eea3bddddddba
mahnaz-ahmadi <84898117+mahnaz-ahmadi@users.noreply.github.com>
2021-06-02 18:54:43 +0430

    Update Mehnaz Ahmedi Calculator.py

--- Mehnaz Ahmedi Calculator.py
+++ Mehnaz Ahmedi Calculator.py
@@ -1,17 +1,28 @@
 print("This is a Smart Calculator")
-print("Select operation.")
-print("1=+")
-print("2=-")
-print("3=*")
-print("4=/")
-selection=input()
-num1=float(input("Enter the First number"))
-num2=float(input("Enter the Second number"))
-if selection == '1':
+num1 = float(input("Enter the First number:"))
+print("Select operation!")
+print("+")
+print("-")
+print("*")
+print("/")
+selection = input()
+if  selection != '+' and selection !='-'  and selection !='/' and selection !='*' :
+    print("invalid input!")
+    print("Now Select valid operation:")
+    print("+")
+    print("-")
+    print("*")
+    print("/")
+    selection = input()
+num2 = float(input("Enter the Second number:"))
+if selection == '+':
     print(num1, "+", num2, "=", num1+num2)
-elif selection == '2':
-            print(num1, "-", num2, "=", num1-num2)
-elif selection == '3':
-            print(num1, "*", num2, "=", num1*num2)
-elif selection == '4':
+elif selection == '-':
+    print(num1, "-", num2, "=", num1-num2)
+elif selection == '*':
+    print(num1, "*", num2, "=", num1*num2)
+elif selection == '/':
+    if num2 != 0:
             print(num1, "/", num2, "=", num1/num2)
+    else:
+            print("invalid division!")
