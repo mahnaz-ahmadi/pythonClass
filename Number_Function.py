22eb00c0d7b766d399ce01c908128b55eb0a7688
mahnaz-ahmadi <84898117+mahnaz-ahmadi@users.noreply.github.com>
2021-06-03 14:50:46 +0430

    Add files via upload

---
+++ Number_Function.py
@@ -0,0 +1,6 @@
+def Number_Function():
+     stringNumber = input("please enter your number:")
+     number = input(int(stringNumber) * 2)
+     print(f"your number * 2 = {number}")
+
+Number_Function()
