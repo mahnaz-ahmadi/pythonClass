59b1d9a5d49ba1834c8ec2844229ff258ee4306b
mahnaz-ahmadi <84898117+mahnaz-ahmadi@users.noreply.github.com>
2021-06-03 14:58:01 +0430

    Update Number_Function.py

--- Number_Function.py
+++ Number_Function.py
@@ -1,6 +1,6 @@
 def Number_Function():
      stringNumber = input("please enter your number:")
-     number = input(int(stringNumber) * 2)
+     number = int(stringNumber) * 2
      print(f"your number * 2 = {number}")
 
 Number_Function()
