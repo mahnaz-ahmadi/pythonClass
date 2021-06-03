cf59c65ed7a8213cd557249ec683e175d75648f3
mahnaz-ahmadi <84898117+mahnaz-ahmadi@users.noreply.github.com>
2021-06-03 16:48:28 +0430

    Add files via upload

---
+++ MostatilOperation_NamedParameter_Function.py
@@ -0,0 +1,13 @@
+def mostatil_operation_namedParameter(tool, arz, dastoor):
+    if dastoor == "mohit":
+        mohit = (int(tool) + int(arz)) * 2
+        print("mohite mostatil = ", mohit)
+    elif dastoor == "masahat":
+        masahat = int(tool) * int(arz)
+        print("masahate mostatil = ", masahat)
+
+
+tool = input("tool ra vared konid: ")
+arz = input("arz ra vared konid: ")
+dastoor = input("dastoore mohit ya masahat ra vared konid: ")
+mostatil_operation_namedParameter(dastoor = dastoor, tool = tool, arz = arz)
