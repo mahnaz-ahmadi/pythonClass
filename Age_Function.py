a6901a7eedd46b2452c6d776e80a75d1cdc9bf35
mahnaz-ahmadi <84898117+mahnaz-ahmadi@users.noreply.github.com>
2021-06-09 13:40:41 +0430

    Update Age_Function.py

--- Age_Function.py
+++ Age_Function.py
@@ -1,4 +1,4 @@
 def Age_Function(age):
     print(f"your age is: {1400 - int(age)}")
-
-Age_Function(input("please enter you birth year:"))
+age = input("please enter you birth year:")
+Age_Function(age)
