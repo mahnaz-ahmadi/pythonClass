60869bb8128710a4be1d938613ce18206198457a
mahnaz-ahmadi <84898117+mahnaz-ahmadi@users.noreply.github.com>
2021-06-10 13:28:15 +0430

    Add files via upload

---
+++ calculator_functions.py
@@ -0,0 +1,44 @@
+print("This is a Smart Calculator")
+num1 = float(input("Enter the First number:"))
+print("Select operation!")
+print("+")
+print("-")
+print("*")
+print("/")
+selection = input()
+if  selection != '+' and selection !='-'  and selection !='/' and selection !='*' :
+    print("invalid input!")
+    print("Now Select valid operation:")
+    print("+")
+    print("-")
+    print("*")
+    print("/")
+    selection = input()
+num2 = float(input("Enter the Second number:"))
+def addition(num1, num2):
+    if selection == '+':
+            result = num1 + num2
+            return result
+
+def subtraction(num1, num2):
+    if selection == '-':
+            result = num1 - num2
+            return result
+
+def multiplication(num1, num2):
+    if selection == '*':
+            result = num1 * num2
+            return result
+
+def division(num1, num2):
+    if selection == '/' and num2 != 0:
+            result = num1 / num2
+            return result
+    else:
+            print("invalid division!")
+
+
+print(addition(num1, num2))
+print(multiplication(num1, num2))
+print(subtraction(num1, num2))
+print(division(num1, num2))
