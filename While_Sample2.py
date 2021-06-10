a3b4446ff5c7294e36010b9742b3e03872b5b40e
mahnaz-ahmadi <84898117+mahnaz-ahmadi@users.noreply.github.com>
2021-06-10 16:03:02 +0430

    Add files via upload

---
+++ While_Sample2.py
@@ -0,0 +1,14 @@
+number = 0
+print(f"{number}")
+while number <= 100:
+    command = input(f"up or down or stop? : ")
+    if command == "up":
+       number += 1
+       print(f"{number}")
+    elif command == "down":
+       number -= 1
+       print(f"{number}")
+    elif command == "stop":
+       break;
+
+print("end of loop")
