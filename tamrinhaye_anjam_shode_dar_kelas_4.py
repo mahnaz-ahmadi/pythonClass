8bc407dd37ae91c8765411c5f25c059a1c381e2e
mahnaz-ahmadi <84898117+mahnaz-ahmadi@users.noreply.github.com>
2021-06-12 16:11:28 +0430

    Add files via upload

---
+++ tamrinhaye_anjam_shode_dar_kelas_4.py
@@ -0,0 +1,65 @@
+# # mostatil va dayere
+# def mostatil(tool, arz, dastoor):
+#     if dastoor == "mohit":
+#         mohit = (tool + arz) * 2
+#         return mohit
+#     elif dastoor == "masahat":
+#         masahat = tool * arz
+#         return masahat
+#     else:
+#         return "balad nistam"
+# def dayere(r, pi):
+#     if dastoor == "mohit":
+#         mohit = r * 2 * pi
+#         return pi
+#     elif dastoor == "masahat":
+#         masahat = r** 2 * pi
+#         return masahat
+#     else:
+#         return "balad nistam"
+#
+# shekl = input("che shekli ?")
+# task = input("che kari ?")
+# if shekl == "mostatil":
+#     tool = int(input("tool ra vared konid"))
+#     arz = int(input("arz ravared konid"))
+# #     print(mostatil(tool, arz, task))
+# shart = True
+# while shart:
+#     print("garmeeeeeeee")
+# start = 0
+# while start <= 100:
+#     if start % 2 == 0:
+#         print(start)
+# #     start += 1
+# start = 1
+# while start <= 50:
+#     if start % 3 == 0:
+#         print(f"{start} bar se bakhshpazire")
+#     else:
+#         print(start)
+#     start += 1
+# start = 0
+# while start <= 100:
+#     command = input("up or down or stop")
+#     if command == "up":
+#         start += 1
+#         print(start)
+#     elif command == "down":
+#         start -= 1
+#         print(start)
+#     elif command == "stop":
+#         break
+# start = 0
+# while  start < 10:
+#     start += 1
+#     if start %2 == 0:
+#         continue
+#     else:
+#         print(start)
+# while  True:
+#     string = input("esmet chie? ")
+#     if string == "nifi":
+#         continue
+#     else:
+#         print(f"salam {string}")
