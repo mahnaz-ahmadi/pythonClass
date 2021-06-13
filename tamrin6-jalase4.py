0905499836228962e307fe60fc4dacd32b920659
mahnaz-ahmadi <84898117+mahnaz-ahmadi@users.noreply.github.com>
2021-06-14 01:57:24 +0430

    Add files via upload

---
+++ tamrin6-jalase4.py
@@ -0,0 +1,8 @@
+len =int(input("How long is this list? "))
+user_list = []
+print(f"plz enter {len} number")
+while len > 0:
+    number = input("enter number: ")
+    user_list.append(number)
+    len -= 1
+input_list =print(user_list)
