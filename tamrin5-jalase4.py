c0d584e223993f5be52272ee2ac7cd95e89602d6
mahnaz-ahmadi <84898117+mahnaz-ahmadi@users.noreply.github.com>
2021-06-14 00:13:43 +0430

    Add files via upload

---
+++ tamrin5-jalase4.py
@@ -0,0 +1,7 @@
+my_class_list = ["reza", "amin", "paniz", "farokh", "mehrana", "meysam",
+"mahnaz", "zeinab", "mohamad", "mobina", "anahita"]
+len_my_class_list = int(len(my_class_list))
+end_list = len_my_class_list - 1
+while end_list >= 0:
+        print(my_class_list[end_list])
+        end_list -= 1
