412d1726bf54d685b6de41a450b2eb36b9ed2a94
mahnaz-ahmadi <84898117+mahnaz-ahmadi@users.noreply.github.com>
2021-06-14 18:51:40 +0430

    Add files via upload

---
+++ tamrin4-1-jalase4.py
@@ -0,0 +1,27 @@
+pattern=float(input("What pattern?"))
+def hop():
+    num1=1
+    while num1<20:
+        if num1%pattern==0:
+            print("Hop")
+            player=input()
+            if float(player)!=num1+1:
+                print("What the hell is this? You lose !")
+                break
+                input()
+            else:
+                num1+=2
+                continue
+        else:
+            print(num1)
+            player=input()
+            if (num1+1)%pattern==0 and player!="hop":
+                print("Huh! You Loser!")
+                break
+            elif (num1+1)%pattern!=0 and player=="hop":
+                print("Huh! You unlocky!")
+                break
+            else:
+                num1+=2
+    print("Ok! That's Enough!")
+hop()
