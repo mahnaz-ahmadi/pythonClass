f4c88386a3926625ea9a60f4aa9afabf04fe5a1a
mahnaz-ahmadi <84898117+mahnaz-ahmadi@users.noreply.github.com>
2021-06-14 12:34:29 +0430

    Update tamrin6-jalase4.py

--- tamrin6-jalase4.py
+++ tamrin6-jalase4.py
@@ -1,8 +1,16 @@
-len =int(input("How long is this list? "))
+length =int(input("How long is this list? "))
 user_list = []
 print(f"plz enter {len} number")
-while len > 0:
-    number = input("enter number: ")
+while length > 0:
+    number = int(input("enter number: "))
     user_list.append(number)
-    len -= 1
-input_list =print(user_list)
+    length -= 1
+input_list =print('original_list : ', user_list)
+
+counter = 0
+unique_user_list = []
+while counter < len(user_list):
+    if user_list[counter] not in unique_user_list:
+       unique_user_list.append(user_list[counter])
+    counter += 1
+print('unique_list : ',  unique_user_list)
