64aa363920a630a3e7be4a1c372811fa1593fe75
mahnaz-ahmadi <84898117+mahnaz-ahmadi@users.noreply.github.com>
2021-06-17 16:15:59 +0430

    Update For_Loop_Sample6.py

--- For_Loop_Sample6.py
+++ For_Loop_Sample6.py
@@ -3,4 +3,4 @@ students =  "mahnaz paniz mehrana farokh mohammad amin reza".split(' ')
 
 for index, item in enumerate(students):
     if (index + 1) % 3 != 0:
-       print(f"{item}")
+       print(f"{index + 1} : {item}")
