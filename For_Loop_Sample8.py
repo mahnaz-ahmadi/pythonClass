14aefc65786110b1c491b501b32004a3baaaafb1
mahnaz-ahmadi <84898117+mahnaz-ahmadi@users.noreply.github.com>
2021-06-17 16:45:01 +0430

    Add files via upload

---
+++ For_Loop_Sample8.py
@@ -0,0 +1,7 @@
+
+
+students =  ["mahnaz", "paniz", "mehrana", "farokh", "zeinab", "reza"]
+gender = ["khnaoom","khnaoom","khnaoom","agha","khnaoom","agha", ]
+
+for st, gender in zip(students, gender) :
+   print(f"salam {st} {gender}")
