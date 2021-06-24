39e43b958ee5ae0e87e3420d079147fb619112db
mahnaz-ahmadi <84898117+mahnaz-ahmadi@users.noreply.github.com>
2021-06-24 15:03:48 +0430

    Update List_zip_without_zip.py

--- List_zip_without_zip.py
+++ List_zip_without_zip.py
@@ -3,7 +3,4 @@ list2 = ["ahmadi", "sedaghat", "zolghadr"]
 
 target_list = []
 for index1, item1 in enumerate(list1):
-    target_list.append(item1)
-    target_list.append(list2[index1])
-
-print(target_list)
+    print(item1 + " " + list2[index1])
