76b2d6c8fc74906b64da85cc95f8c6edb64918b8
mahnaz-ahmadi <84898117+mahnaz-ahmadi@users.noreply.github.com>
2021-07-01 02:08:46 +0430

    Add files via upload

---
+++ tamrin2-jalase5.py
@@ -0,0 +1,6 @@
+list1 = ["mahnaz", "paniz", "anahita"]
+list2 = ["ahmadi", "sedaghat", "zolghadr"]
+
+target_list = []
+for index1, item1 in enumerate(list1):
+    print(item1 + " " + list2[index1])
