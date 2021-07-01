f7dc323af8901166f82337729aaf141dd0985568
mahnaz-ahmadi <84898117+mahnaz-ahmadi@users.noreply.github.com>
2021-07-01 16:04:44 +0430

    Add files via upload

---
+++ Class_Sample2.py
@@ -0,0 +1,25 @@
+
+class Cat:
+    name = ""
+    age = 0
+    color = ""
+
+    def hello(self):
+        print("miou miou")
+
+
+jack = Cat()
+jack.name = "jack"
+jack.age = 4
+jack.color = "white"
+
+jo = Cat()
+jo.name = "jo"
+jo.age = 4
+jo.color = "black"
+
+print(jack.name)
+jack.hello();
+
+print(jo.name)
+jo.hello();
