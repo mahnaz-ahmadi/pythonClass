e0921771983ddf33bc84be882a5b9de3bddf755d
mahnaz-ahmadi <84898117+mahnaz-ahmadi@users.noreply.github.com>
2021-07-08 15:44:37 +0430

    Add files via upload

---
+++ Class_Inheritance_Sample1.py
@@ -0,0 +1,21 @@
+
+class Animal:
+        def __init__(self, name, age, weight):
+            self.name = name
+            self.age = age
+            self.weight = weight
+
+        def move(self):
+            print('i can move')
+
+        def make_noise(self):
+            print('this is my voice')
+
+
+class  Reptile(Animal):
+    def __init__(self, name, age, weight, home):
+        super().__init__(name, age, weight)
+        self.home = home
+
+turtle = Reptile('lucky', 300, 400, 'land')
+print(turtle.home)
