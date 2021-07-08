649f4d912b9ad32f81b5184acdf843edbef06258
mahnaz-ahmadi <84898117+mahnaz-ahmadi@users.noreply.github.com>
2021-07-08 15:44:24 +0430

    Add files via upload

---
+++ Class_Inheritance_Sample2.py
@@ -0,0 +1,17 @@
+
+class A:
+    def __init__(self, name):
+        self.name = name
+
+
+class  B(A):
+    def __init__(self, name):
+        super().__init__(name)
+
+class  C(B):
+    def __init__(self, name):
+        super().__init__(name)
+
+
+child = C("mahnaz")
+print(child.name)
